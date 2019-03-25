da3eea33ab75b417e0ceb0b0e1d8761cc63f6f35
Ivan Burnaev <ivan.biirus@gmail.com>
2019-03-25 22:08:28 +0300

    build(dist): trying to figure out how to change publish root of npm package

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,38 +1,30 @@
 const path = require("path");
 
+console.log(process.env.NODE_ENV);
+
 module.exports = {
   webpack (config, options) {
-    config.resolve.alias["iburn"] = path.join(__dirname, "../src");
-    config.resolve.alias["docs"] = __dirname;
+    if (process.env.NODE_ENV === "development") {
+      config.resolve.alias["iburn"] = path.join(__dirname, "../src");
+      config.resolve.alias["components"] = path.join(
+        __dirname,
+        "../src/components"
+      );
 
-    config.resolve.alias["components"] = path.join(
-      __dirname,
-      "../src/components"
-    );
+      config.module.rules = config.module.rules.map(rule => {
+        if (!rule.test.test(".js")) {
+          return rule;
+        }
 
-    config.module.rules = config.module.rules.map(rule => {
-      if (!rule.test.test(".js")) {
+        rule.include.push(path.resolve(__dirname, "../src"));
         return rule;
-      }
+      });
+    }
 
-      rule.include.push(path.resolve(__dirname, "../src"));
-      return rule;
-    });
+    config.resolve.alias["docs"] = __dirname;
 
     return config;
   },
 
-  exportPathMap: async function (defaultPathMap) {
-    console.log(defaultPathMap);
-
-    return {
-      // ...defaultPathMap,
-      "/api/avatar": { page: "/api", query: { component: "Avatar" } },
-      "/api/avatar/stack": {
-        page: "/api",
-        query: { component: "Avatar/Stack" },
-      },
-      "/demo/avatar-stack": { page: "/demo/avatar-stack" },
-    };
-  },
+  target: "serverless",
 };
